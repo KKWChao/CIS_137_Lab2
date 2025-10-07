627f64670bfbc19bcceb80d73cefddc8f3b7814f
Kelvin <kelvinchao@protonmail.com>
2025-10-07 15:40:22 -0700

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Kelvin Chao on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
